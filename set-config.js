92a1f0e51d177a7b6b23dcc417cb8720af420142
tsctsc6 <tsctsc6@126.com>
2025-06-09 19:05:42 +0800

    set bundle.targets

--- set-config.js
+++ set-config.js
@@ -3,4 +3,5 @@ import { readFileSync, writeFileSync } from 'fs';
 const config = JSON.parse(readFileSync('src-tauri/tauri.conf.json'));
 config.identifier = "com.tsctsc6.sample-svelte";
 config.build.frontendDist = "../dist";
+config.bundle.targets = ["msi"];
 writeFileSync('src-tauri/tauri.conf.json', JSON.stringify(config, null, 2));
