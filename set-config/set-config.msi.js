ea5e9aca00b900a34ee7d40a8865fbb744e84693
tsctsc6 <tsctsc6@126.com>
2025-06-09 22:51:05 +0800

    fix: 'wix' was unexpected

--- set-config/set-config.msi.js
+++ set-config/set-config.msi.js
@@ -3,10 +3,6 @@ import { readFileSync, writeFileSync } from 'fs';
 const config = JSON.parse(readFileSync('src-tauri/tauri.conf.json'));
 
 config.bundle.targets = ["msi"];
-if (config.wix === undefined)
-{
-    config.wix = {};
-}
-config.wix.upgradeCode = "833ad97c-91d0-47cb-83b2-2e71181c0584";
+config.wixConfig.upgradeCode = "833ad97c-91d0-47cb-83b2-2e71181c0584";
 
 writeFileSync('src-tauri/tauri.conf.json', JSON.stringify(config, null, 2));
