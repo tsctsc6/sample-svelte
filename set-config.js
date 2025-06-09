f357d7eed8b268b092bf76bd200ed6837b8e3b7e
tsctsc6 <tsctsc6@126.com>
2025-06-09 16:18:26 +0800

    fix: error filename

--- set-config.js
+++ set-config.js
@@ -2,4 +2,4 @@ import { readFileSync, writeFileSync } from 'fs';
 
 const config = JSON.parse(readFileSync('src-tauri/tauri.conf.json'));
 config.identifier = "com.tsctsc6.sample-svelte";
-writeFileSync('config.json', JSON.stringify(config, null, 2));
+writeFileSync('src-tauri/tauri.conf.json', JSON.stringify(config, null, 2));
