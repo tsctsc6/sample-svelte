f20272cb51b4a3620ef0efd27cdeeef42f08ca58
tsctsc6 <tsctsc6@126.com>
2025-06-09 17:12:03 +0800

    set frontendDist

--- set-config.js
+++ set-config.js
@@ -2,4 +2,5 @@ import { readFileSync, writeFileSync } from 'fs';
 
 const config = JSON.parse(readFileSync('src-tauri/tauri.conf.json'));
 config.identifier = "com.tsctsc6.sample-svelte";
+config.build.frontendDist = "../dist";
 writeFileSync('src-tauri/tauri.conf.json', JSON.stringify(config, null, 2));
