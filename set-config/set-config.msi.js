6ff0bc1dd2c42a9dccb14576503a5de41cdf0642
tsctsc6 <tsctsc6@126.com>
2025-06-09 22:55:13 +0800

    init config.wixConfig

--- set-config/set-config.msi.js
+++ set-config/set-config.msi.js
@@ -3,6 +3,10 @@ import { readFileSync, writeFileSync } from 'fs';
 const config = JSON.parse(readFileSync('src-tauri/tauri.conf.json'));
 
 config.bundle.targets = ["msi"];
+if (config.wixConfig === undefined)
+{
+    config.wixConfig = {};
+}
 config.wixConfig.upgradeCode = "833ad97c-91d0-47cb-83b2-2e71181c0584";
 
 writeFileSync('src-tauri/tauri.conf.json', JSON.stringify(config, null, 2));
