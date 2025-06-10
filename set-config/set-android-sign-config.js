a40418ba3d015b861f5a570b92a7d0ef9ef27b6d
tsctsc6 <tsctsc6@126.com>
2025-06-10 11:57:30 +0800

    feat: set android sign config

---
+++ set-config/set-android-sign-config.js
@@ -0,0 +1,55 @@
+import { readFileSync, writeFileSync } from 'fs';
+import { exit } from 'process';
+
+const filePath = './src-tauri/gen/android/app/build.gradle.kts';
+
+var content = readFileSync(filePath).toString();
+
+var insertPoint = findInsertPoint('\r\n', content);
+content = content.substring(0, insertPoint) + 'import java.io.FileInputStream\r\n' + content.substring(insertPoint);
+
+const signingConfigs = `
+    signingConfigs {
+        create("release") {
+            val keystorePropertiesFile = rootProject.file("key.properties")
+            val keystoreProperties = Properties()
+            if (keystorePropertiesFile.exists()) {
+                keystoreProperties.load(FileInputStream(keystorePropertiesFile))
+            }
+
+            keyAlias = keystoreProperties["keyAlias"] as String
+            keyPassword = keystoreProperties["keyPassword"] as String
+            storeFile = file(keystoreProperties["storeFile"] as String)
+            storePassword = keystoreProperties["storePassword"] as String
+        }
+    }
+`;
+
+var insertPoint = findInsertPoint('    buildTypes {', content, 'before');
+content = content.substring(0, insertPoint) + signingConfigs + content.substring(insertPoint);
+
+var insertPoint = findInsertPoint('getByName("release") {\r\n', content, 'after');
+content = content.substring(0, insertPoint) + '            signingConfig = signingConfigs.getByName("release")\r\n' + content.substring(insertPoint);
+
+writeFileSync(filePath, content);
+
+
+function findInsertPoint(pattern, content, mode = 'after')
+{
+    var patternIndex = content.indexOf(pattern);
+    if (patternIndex === -1)
+    {
+        console.error(`Can't find pattern: ${pattern}`)
+        exit;
+    }
+    var insertPoint = -1;
+    if (mode === 'before')
+    {
+        insertPoint = patternIndex;
+    }
+    else if (mode === 'after')
+    {
+        insertPoint = patternIndex + pattern.length;
+    }
+    return insertPoint;
+}
